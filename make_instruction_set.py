e4d468aba8cf0f6658293bb4f0f8fa90a9094e4d
Gregory Propf <gpropf@gmail.com>
2018-06-06 02:21:43 -0700

    Added instruction_set.json to define instruction set and parser to emit both C++ header file and python assembler dict structures.

---
+++ make_instruction_set.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+
+# make_instruction_set.py
+# Script to process a JSON format file representing the VM instruction set and produce various
+# things such as Python data structures for the Python assembler and C++ header code
+# for the C++ source code.
+
+import json
+
+def emitCode(instructions, codeType = "C++"):
+    currentBase = ""
+    offset = 0
+    for instr in instructions:
+        base = instr["base"]
+        if base != currentBase:
+            currentBase = base
+            offset = 0
+        else:
+            offset = offset + 1
+        print (instr["mnemonic"] + " = " + base + " + " + str(offset) + ",")
+    
+
+
+with open("instruction_set.json", "r") as read_file:
+    data = json.load(read_file)
+    emitCode(data["instructions"]["multiWidth"])
+
