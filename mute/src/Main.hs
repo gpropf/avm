835ceec24aa2927a0a23ac969ce94fcef4a6a540
Gregory Propf <gpropf@gmail.com>
2018-07-26 02:24:38 -0700

    Got a rudimentary toy compiler working using the ST monad.

--- mute/src/Main.hs
+++ mute/src/Main.hs
@@ -11,11 +11,19 @@ import Text.ParserCombinators.Parsec.Language
 import qualified Text.ParserCombinators.Parsec.Token as Token
 --import FunctionsAndTypesForParsing
 
+import Control.Monad.ST
+import Data.STRef
+
+
+
 data IOPinType = AnalogIn | AnalogOut | DigitalIn | DigitalOut deriving (Show)
 
 data IOPin = IOPin { pinType :: IOPinType
                    , pinNum :: Int
                    } deriving (Show)
+
+data Var = Var { varType :: [Char], location :: Int } deriving (Show)
+
 
 eol :: GenParser Char st Char
 eol = char '\n'
@@ -34,9 +42,25 @@ mnum = do
 regularParse :: Parser a -> String -> Either ParseError a
 regularParse p = parse p ""
 
+whitespace = void $ many $ oneOf " \n\t"
+testParsingFromFile fn = parseFromFile  (sepBy1 pindef whitespace) fn
+
+handleErrors eparseList =
+  case eparseList of
+    Left err -> Left err
+    Right pl ->
+      Right $ firstPass pl
+
+firstPass parseList = runST $ do
+  varList <- newSTRef []
+  forM_ parseList $ \v -> do
+    modifySTRef varList $ ((:) . (pinNum)) v
+  readSTRef varList
+
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  parseResult <- testParsingFromFile "testpins.mute"
+  putStrLn $ show $ parseResult
 
 ain :: GenParser Char st IOPinType
 ain = do
