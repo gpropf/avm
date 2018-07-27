1514603846251b7fa45897cf67e649ffddc818eb
Gregory Propf <gpropf@gmail.com>
2018-07-26 21:32:40 -0700

    Made a lot of progress on the compiler. We now have a full set of compiler abstractions to represent internal program state as we compile.

--- mute/src/Main.hs
+++ mute/src/Main.hs
@@ -11,11 +11,32 @@ import Text.ParserCombinators.Parsec.Language
 import qualified Text.ParserCombinators.Parsec.Token as Token
 --import FunctionsAndTypesForParsing
 
+import Control.Monad.ST
+import Data.STRef
+
+
+
 data IOPinType = AnalogIn | AnalogOut | DigitalIn | DigitalOut deriving (Show)
 
 data IOPin = IOPin { pinType :: IOPinType
                    , pinNum :: Int
                    } deriving (Show)
+
+
+data Func = Func { fname :: [Char], args :: [Var] , asm :: [Char]} deriving (Show)
+data Var = Var { vname :: [Char], varType :: [Char], location :: Int } deriving (Show)
+data CompilerContext = CompilerContext { vars :: [Var], functions :: [Func], contextAsm :: [Char] } | NullContext deriving (Show)
+data CompilerMetaData = CompilerMetaData { ip :: Int  , hp :: Int, globalContext :: CompilerContext } deriving (Show)
+
+initCC = CompilerContext [] [] ""
+initCMD = CompilerMetaData 0 0 NullContext
+addVarToCC v cc =
+  case cc of
+    NullContext -> addVarToCC v initCC
+    cc -> cc { vars = v : (vars cc) }
+
+
+swapCC incc cmd = cmd { globalContext = incc }
 
 eol :: GenParser Char st Char
 eol = char '\n'
@@ -34,9 +55,37 @@ mnum = do
 regularParse :: Parser a -> String -> Either ParseError a
 regularParse p = parse p ""
 
+whitespace = void $ many $ oneOf " \n\t"
+testParsingFromFile fn = parseFromFile  (sepBy1 pindef whitespace) fn
+
+handleErrors eparseList =
+  case eparseList of
+    Left err -> Left err
+    Right pl ->
+      Right $ firstPassCC pl
+
+firstPass parseList = runST $ do
+  varList <- newSTRef []
+  forM_ parseList $ \v -> do
+    modifySTRef varList $ ((:) . (pinNum)) v
+  readSTRef varList
+
+firstPassCC parseList = runST $ do
+  cmd <- newSTRef initCMD
+  --
+  forM_ parseList $ \v -> do
+    cmd' <- readSTRef cmd
+    let newvar = Var ((show (pinType v)) ++ (show (pinNum v))) "PIN" 0
+        newcc = globalContext cmd'
+        newcc' = addVarToCC newvar newcc
+    modifySTRef cmd $ (swapCC newcc') . (\cmd -> cmd {ip = (ip cmd) + 2})
+  readSTRef cmd
+
+
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  parseResult <- testParsingFromFile "testpins.mute"
+  putStrLn $ show $ parseResult
 
 ain :: GenParser Char st IOPinType
 ain = do
