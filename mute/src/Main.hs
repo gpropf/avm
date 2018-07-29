d0986c4dda3d1239b9e31e6702e581c280433df2
Gregory Propf <gpropf@gmail.com>
2018-07-29 02:57:07 -0700

    parser now more tolerant of spaces in the list of pins.

--- mute/src/Main.hs
+++ mute/src/Main.hs
@@ -5,6 +5,7 @@ import Mute
 import Text.ParserCombinators.Parsec
 import Text.ParserCombinators.Parsec.Expr
 import Text.ParserCombinators.Parsec.Language
+import Control.Monad
 
 testParsingFromFile :: String -> IO (Either ParseError [Mute.IOPin])
 testParsingFromFile fn = parseFromFile pindefs fn
@@ -19,5 +20,7 @@ handleErrors eparseList =
 
 main :: IO ()
 main = do
-  parseResult <- testParsingFromFile "testpins.mute"
+  parseResult <- liftM handleErrors (parseFromFile parseProgram "testpins.mute") 
   putStrLn $ show $ parseResult
+  --parseResult <- liftM handleErrors (parseFromFile varName "testpins.mute") 
+--  putStrLn $ show $ parseResult
--- mute/src/Mute.hs
+++ mute/src/Mute.hs
@@ -1,4 +1,4 @@
-module Mute (pindefs, firstPassCC, IOPin) where
+module Mute (pindefs, firstPassCC, IOPin(..), Var(..), IOPinType(..), emit, parseProgram, num, mnum, aout, ain, din, dout, varName) where
 
 
 --import Parsec
@@ -7,12 +7,20 @@ import Control.Monad
 import Text.ParserCombinators.Parsec
 import Text.ParserCombinators.Parsec.Expr
 import Text.ParserCombinators.Parsec.Language
+import Text.Parsec.Char
+--import Text.Parsec.Prim
 --import Text.Parsec.String (oneOf, char, digit, satisfy)
 import qualified Text.ParserCombinators.Parsec.Token as Token
---import FunctionsAndTypesForParsing
 
 import Control.Monad.ST
 import Data.STRef
+
+
+class Emitter a where
+  emit :: a -> String
+
+instance Emitter Var where
+  emit v = "BIND" ++ (varType v) ++ " " ++ (vname v) ++ " " ++ (show (location v)) ++ "\n"
 
 
 
@@ -26,7 +34,7 @@ data IOPin = IOPin { pinType :: IOPinType
 
 data Func = Func { fname :: [Char], args :: [Var] , asm :: [Char]} deriving (Show)
 data Var = Var { vname :: [Char], varType :: [Char], location :: Int } deriving (Show)
-data CompilerContext = CompilerContext { vars :: [Var], functions :: [Func], contextAsm :: [Char] } | NullContext deriving (Show)
+data CompilerContext = CompilerContext { vars :: [Var], subContexts :: [CompilerContext], contextAsm :: [Char] } | NullContext deriving (Show)
 data CompilerMetaData = CompilerMetaData { ip :: Int  , hp :: Int, globalContext :: CompilerContext } deriving (Show)
 
 initCC = CompilerContext [] [] ""
@@ -39,13 +47,8 @@ addVarToCC v cc =
 
 swapCC incc cmd = cmd { globalContext = incc }
 
-
-
-
-
 eol :: GenParser Char st Char
 eol = char '\n'
-
 
 num :: Parser Integer
 num = do
@@ -61,7 +64,7 @@ regularParse :: Parser a -> String -> Either ParseError a
 regularParse p = parse p ""
 
 whitespace = void $ many $ oneOf " \n\t"
-testParsingFromFile fn = parseFromFile  (sepBy1 pindef whitespace) fn
+--testParsingFromFile fn = parseFromFile  (sepBy pindef whitespace) fn
 
 handleErrors eparseList =
   case eparseList of
@@ -83,24 +86,34 @@ firstPassCC parseList = runST $ do
   forM_ parseList $ \v -> do
     cmd' <- readSTRef cmd
     let newvar = Var ((show (pinType v)) ++ (show (pinNum v))) "PIN" 0
-        newcc = globalContext cmd'
-        newcc' = addVarToCC newvar newcc
-    modifySTRef cmd $ (swapCC newcc') . (\cmd -> cmd {ip = (ip cmd) + 2})
+        newcc = globalContext cmd'        
+        newcc' = addVarToCC newvar $
+          case newcc of
+            NullContext -> addVarToCC newvar (initCC) { contextAsm = emit newvar }
+            globalCC -> globalCC { contextAsm = (contextAsm globalCC) ++ emit newvar}
+--        newcc' = addVarToCC newvar newcc
+    modifySTRef cmd $ (swapCC newcc') . (\cmd -> cmd { ip = (ip cmd) + 2} )
   readSTRef cmd
 
-main :: IO ()
-main = do
-  parseResult <- testParsingFromFile "testpins.mute"
-  putStrLn $ show $ parseResult
+
 
 ain :: GenParser Char st IOPinType
 ain = do
   pin <- string "AI"
   return AnalogIn
 
+aout :: GenParser Char st IOPinType
 aout = do
   pin <- string "AO"
   return AnalogOut
+
+{-
+aout2 :: Text.Parsec.Prim.ParsecT
+         [Char] u Data.Functor.Identity.Identity IOPinType
+aout2 = do
+  pin <- string "AO"
+  return AnalogOut
+-}
 
 analog = do
   pin <- try aout <|> ain
@@ -126,4 +139,16 @@ pindef = do
   return (IOPin pinType (fromIntegral pinNum))
 
 pindefs = do
-  sepBy1 pindef whitespace
+  endBy pindef whitespace
+
+varName :: GenParser Char st [Char]
+varName = do
+  sc <- lower
+  nextLetters <- many ( alphaNum <|> char '_' )
+  return $ sc : nextLetters
+
+
+
+parseProgram = do
+  pd <- pindefs
+  return pd
